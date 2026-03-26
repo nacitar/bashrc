46ddd410e8385da77b3c7d30c40a44f8ef0317e2
Jacob McIntosh <nacitar.sevaht@gmail.com>
2026-03-26 11:26:11 -0600

    Adjusted python aliases

--- addons/python-aliases.sh
+++ addons/python-aliases.sh
@@ -1,13 +1,3 @@
-checks() {
-    local directory=${PWD}
-    while [[ -n ${directory} ]]; do
-        local script_path=${directory}/checks
-        if [[ -x ${script_path} ]]; then
-            "${script_path}" "${@}"
-            return
-        fi
-        directory=${directory%/*}
-    done
-    >&2 echo "Could not find 'checks' script in CWD or any parent."
-    return 1
-}
+# python development
+alias checks="ns_run_ancestor checks"
+alias venv-shell="ns_run_ancestor venv-shell"
